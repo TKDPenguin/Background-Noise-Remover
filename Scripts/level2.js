db60a2a651f61a7d1de040925198013f3194d4bc
aBacIdEe <maxgong2007@gmail.com>
2024-06-11 09:26:00 -0400

    new level

--- Scripts/level2.js
+++ Scripts/level2.js
@@ -1,56 +1,67 @@
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite,
-    Mouse = Matter.Mouse,
-    MouseConstraint = Matter.MouseConstraint;
+const {Engine, Bodies, Mouse, MouseConstraint, Composite, Render, Runner, Constraint, Collision} = Matter;
 
-var canvas = document.querySelector('canvas');
-var height = canvas.height;
-var width = canvas.width;
+const boxes = [];
+const boundaries = [];
+const shotBirds = [];
+let bird, pig, slingshot;
+let world, engine, mConstraint, render, runner, constraint, collision;
+let canvas, width, height;
+let numOfShots = 3;
+let numOfPigs = 1;
+let index = 0;
+let bodies = [];
 
-// create an engine
-var engine = Engine.create(canvas);
-
-// create a renderer
-var render = Render.create({
-    element: document.body,
-    canvas: canvas,
-    engine: engine,
-    options: {
-        width: width,
-        height: height,
-        wireframes: false,
-        background: 'rgb(255,255,255)'
+function setup() {
+    canvas = document.querySelector('canvas');
+    width = window.innerWidth;
+    height = window.innerHeight * .95;
+    canvas.width = width;
+    canvas.height = height;
+    engine = Engine.create();
+    engine.gravity.y = .7;
+    world = engine.world;
+    for (let j = 0; j < 5; j++) {
+        for (let i = 0; i < 7; i++) {
+            boxes[i*j+i] = new Box(0.75*width-j*50, height-i*75, 50, 75);
+        }
     }
-});
+    boundaries[0] = new staticBox(width+25, height/2, 50, height);
+    boundaries[1] = new staticBox(width/2, -25, width, 50);
+    boundaries[2] = new staticBox(-25, height/2, 50, height);
+    boundaries[3] = new staticBox(width/2, height+25, width, 50);
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80, {
-    render: {
-        fillStyle: 'purple'
+    pig = new Pig(width*.7, height*.4, 25);
+
+    bird = new Bird(width*.12, height*.85, 15);
+
+    slingshot = new Slingshot(width*.12, height*.85, bird.body);
+
+    const mouse = Mouse.create(canvas.elt);
+    console.log(mouse);
+    const options = {
+        mouse: mouse,
+        constraint: {
+            render: {
+                visible: false
+            }
+        },
+        collisionFilter: {
+            mask: 0b1
+        }
     }
-});
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(width/2, height-30, width, 60, { isStatic: true });
-
-const mouse = Mouse.create(document.body);
-const options = {
-    mouse: mouse
+    mConstraint = MouseConstraint.create(engine, options);
+    Composite.add(world, mConstraint);
+    render = Render.create({
+        element: document.body,
+        canvas: canvas,
+        engine: engine,
+        options: {
+            width: width,
+            height: height,
+            wireframes: false,
+            background: 'rgb(255,255,255)'
+        }
+    });
+    runner = Runner.create();
 }
-mConstraint = MouseConstraint.create(engine, options);
-Composite.add(engine.world, mConstraint);
 
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
-
-// run the renderer
-Render.run(render);
-
-// create runner
-var runner = Runner.create();
-
-// run the engine
-Runner.run(runner, engine);
